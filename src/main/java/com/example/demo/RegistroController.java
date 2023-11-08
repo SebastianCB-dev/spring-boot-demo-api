3bc7d72cf190baacf8f5cc34d3b9a61db5551da6
Sebastián Carrillo <sebastian.carrillob01@gmail.com>
2023-11-07 20:30:03 -0500

    Feat: Add model User to register

--- src/main/java/com/example/demo/RegistroController.java
+++ src/main/java/com/example/demo/RegistroController.java
@@ -1,18 +1,36 @@
 package com.example.demo;
 
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
+import com.example.demo.Model.User;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+
+@RestController
 @RequestMapping("/api")
 public class RegistroController {
 
     @PostMapping("/register")
-    @ResponseBody
-    public String registrar() {
-        // Lógica para procesar la solicitud de registro
-        return "Registro exitoso"; // Puedes personalizar la respuesta según tus necesidades
+    public ResponseEntity<Object> register(@RequestBody User userData) {
+        // Accede a los datos recibidos en formato JSON
+        String gmail = userData.getGmail();
+        String password = userData.getPassword();
+        System.out.println(gmail);
+        System.out.println(password);
+        // Realiza las validaciones de los campos
+        if (gmail == null || password == null || gmail == "" || password == "") {
+            // Crear un objeto JSON de error
+            return new ResponseEntity<>("{\"message\":\"Datos incorrectos\",\"status\":\"error\"}", HttpStatus.BAD_REQUEST);
+        }
+
+        // Realiza la lógica de registro con los datos
+        // ...
+
+        // Crear un objeto JSON de éxito
+        return new ResponseEntity<>("{\"message\":\"Registro exitoso\",\"status\":\"success\"}", HttpStatus.OK);
     }
 }
