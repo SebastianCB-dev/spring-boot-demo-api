d4037ea8f903c8fac35f6a9060fdeaa8b368149c
Sebastián Carrillo <sebastian.carrillob01@gmail.com>
2023-11-09 16:53:25 -0500

    Feat: Update EntityUser

--- src/main/java/com/example/demo/Model/User.java
+++ src/main/java/com/example/demo/Model/User.java
@@ -3,7 +3,8 @@ package com.example.demo.Model;
 public class User {
   private String gmail;
   private String password;
-
+  private String name;
+  private String surname;
 
   public String getGmail() {
     return gmail;
@@ -20,4 +21,21 @@ public class User {
   public void setPassword(String password) {
     this.password = password;
   }
+
+  public String getName() {
+    return name;
+  }
+
+  public void setName(String name) {
+    this.name = name;
+  }
+
+  public String getSurname() {
+    return surname;
+  }
+
+  public void setSurname(String surname) {
+    this.surname = surname;
+  }
+
 }
--- src/main/java/com/example/demo/RegistroController.java
+++ src/main/java/com/example/demo/RegistroController.java
@@ -19,10 +19,15 @@ public class RegistroController {
         // Accede a los datos recibidos en formato JSON
         String gmail = userData.getGmail();
         String password = userData.getPassword();
+        String name = userData.getName();
+        String surname = userData.getSurname();
         System.out.println(gmail);
         System.out.println(password);
         // Realiza las validaciones de los campos
-        if (gmail == null || password == null || gmail == "" || password == "") {
+        if (gmail == "" || gmail == null || 
+            password == "" || password == null || 
+            name == "" || name == null || 
+            surname == "" || surname == null) {
             // Crear un objeto JSON de error
             return new ResponseEntity<>("{\"message\":\"Datos incorrectos\",\"status\":\"error\"}", HttpStatus.BAD_REQUEST);
         }
